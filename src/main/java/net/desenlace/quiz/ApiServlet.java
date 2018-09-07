8425bbe42d30f00eb66cfb94b041ca14383b432b
root <desenlace.net@gmail.com>
2018-09-07 16:33:24 +0000

    Añadidos soporte para errores de api en json

--- src/main/java/net/desenlace/quiz/ApiServlet.java
+++ src/main/java/net/desenlace/quiz/ApiServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
 
+import javax.json.Json;
+
 public abstract class ApiServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = -9089534560668756167L;
@@ -19,15 +21,30 @@ public abstract class ApiServlet extends HttpServlet {
         try{
             Connection db = DriverManager.getConnection(DbData.DB_URL, DbData.DB_USER, DbData.DB_PASS);
             response.setHeader("Access-Control-Allow-Origin", "*");
-			response.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
             response.getWriter().append(postResponse(new Request(request), db));
         } catch (SQLException e){
             response.getWriter().append(e.getMessage());
         } catch (ApiException e){
             response.setStatus(e.getErrorCode());
-            response.getWriter().append(e.getMessage());
+            response.getWriter().append(Json.createObjectBuilder().add("error", e.getMessage()).build().toString());
         }
     }
     
-    protected abstract String postResponse(Request request, Connection connection) throws SQLException;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	        try{
+        	    	Connection db = DriverManager.getConnection(DbData.DB_URL, DbData.DB_USER, DbData.DB_PASS);
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			response.setCharacterEncoding("UTF-8");
+	        	response.getWriter().append(getResponse(new Request(request), db));
+	        } catch (SQLException e){
+	           	response.getWriter().append(e.getMessage());
+	        } catch (ApiException e){
+	           	 response.setStatus(e.getErrorCode());
+	            	 response.getWriter().append(Json.createObjectBuilder().add("error", e.getMessage()).build().toString());
+        	}
+	}
+
+    protected String postResponse(Request request, Connection connection) throws SQLException{throw new ApiException(405, "Metodo no permitido");}
+    protected String getResponse(Request request, Connection connection) throws SQLException{throw new ApiException(405, "Metodo no permitido");}
 }
