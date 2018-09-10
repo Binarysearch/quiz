1e0a2c21764a80e2ee4447f225ba506b386e887e
Antonio Ruiz <desenlace.net@gmail.com>
2018-09-10 19:24:03 +0200

    Crear pregunta admite 2, 3 y 4 respuestas

--- src/main/java/net/desenlace/quiz/Request.java
+++ src/main/java/net/desenlace/quiz/Request.java
@@ -10,12 +10,33 @@ public class Request {
         this.request = request;
     }
 
+    /**
+     * Obtiene el valor de un parametro de la peticion.
+     * @param paramName El nombre del parametro
+     * @return El valor del parametro
+     * @throws una ApiException si en la peticion no existe el parametro indicado.
+     */
     public String getString(String paramName){
         String result = this.request.getParameter(paramName);
         if(result == null){
             throw new ApiException(400, "El parametro '"+paramName+"' es obligatorio");
         }
         return result;
+    }
+
+    /**
+     * Obtiene el valor de un parametro de la peticion.
+     * @param paramName El nombre del parametro
+     * @param defaultValue El valor por defecto
+     * @return El valor del parametro si existe. Si el parametro no existe la fucnion devuelve el 
+     * valor indicado en defaultValue
+     */
+    public String getString(String paramName, String defaultValue){
+        String result = this.request.getParameter(paramName);
+        if(result != null){
+            return result;
+        }
+        return defaultValue;
     }
 
 	public int getInt(String paramName) {
