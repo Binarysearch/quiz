51086342f195aa782a3765b3f9eef7649cc18abe
Ionut Morariu <ionut.morariu.c@gmail.com>
2018-09-13 17:52:33 +0200

    Box styled component

---
+++ src/main/webapp/react-app/src/Components/Box.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledBox = styled.div`
+	background-color: white;
+	padding: 10px 30px;
+	border: 1px solid #cdcdcd;
+	border-radius: 5px;
+	box-sizing: border-box;
+	box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.1);
+	width: 30%;
+	margin: 0 auto;
+	margin-top: 50px;
+`;
+
+export class Box extends Component {
+	render() {
+		return <StyledBox>{this.props.children}</StyledBox>;
+	}
+}
+
+export default Box;
